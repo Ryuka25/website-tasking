53e7234e8b12b650baa0c3e83bf2c9342e8b29d8
Lovanirina R <lovanirina.ran@gmail.com>
2025-06-01 06:26:22 +0300

    feat: add header section with call-to-action and image to landing page 1

--- pages/landing-page-1/index.vue
+++ pages/landing-page-1/index.vue
@@ -32,5 +32,32 @@
         </button>
       </div>
     </nav>
+
+    <header class="min-h-screen flex justify-center items-center pt-36 pb-7">
+      <div
+        class="grid grid-cols-1 md:grid-cols-2 items-center max-w-6xl mx-auto px-7"
+      >
+        <div class="mb-10 md:mb-0">
+          <h1 class="text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
+            Manage <br />
+            your
+            <span class="font-bold">tasks</span>
+            quickly
+          </h1>
+          <p class="mb-5 mt-7 max-w-md text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            nesciunt dicta consequuntur earum culpa voluptatibus!
+          </p>
+          <a
+            href="#"
+            class="bg-blue-700 hover:bg-blue-800 transition-colors text-white py-3 px-7 rounded-md mr-4 mt-1 inline-block text-lg"
+          >
+            Get Started
+          </a>
+          <a href="#" class="font-bold">Learn More</a>
+        </div>
+        <NuxtImg src="/assets/landing-page-1/img1.svg" />
+      </div>
+    </header>
   </div>
 </template>
