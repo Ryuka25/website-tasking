bcd711f5e7598322dc6d77c5cebf5fd4701ee395
Lovanirina R <lovanirina.ran@gmail.com>
2025-06-01 06:26:22 +0300

    feat: add articles section with blog previews to landing page 1

--- pages/landing-page-1/index.vue
+++ pages/landing-page-1/index.vue
@@ -167,5 +167,57 @@
         </div>
       </div>
     </section>
+    <section class="bg-white">
+      <div class="max-w-6xl mx-auto px-7 py-10">
+        <div class="mb-14 text-center">
+          <p class="text-blue-600 text-lg">Articles</p>
+          <h1 class="font-bold text-4xl">Articles From Tasking</h1>
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <div class="border border-gray-200 rounded-lg p-4">
+            <NuxtImg src="/assets/landing-page-1/blog1.jpg" class="mb-4" />
+            <p class="text-gray-400 text-lg">Technology</p>
+            <h2 class="text-2xl font-bold my-4">
+              Bradford assay is an addition to comassie...
+            </h2>
+            <a
+              href="#"
+              class="flex justify-start items-center gap-4 hover:text-blue-600 transition-colors mb-2"
+            >
+              <span class="font-bold">Read More</span>
+              <Icon name="lucide:arrow-right" class="text-blue-600" />
+            </a>
+          </div>
+          <div class="border border-gray-200 rounded-lg p-4">
+            <NuxtImg src="/assets/landing-page-1/blog2.jpg" class="mb-4" />
+            <p class="text-gray-400 text-lg">Real Estate</p>
+            <h2 class="text-2xl font-bold my-4">
+              New economy, Real estate investment will be...
+            </h2>
+            <a
+              href="#"
+              class="flex justify-start items-center gap-4 hover:text-blue-600 transition-colors mb-2"
+            >
+              <span class="font-bold">Read More</span>
+              <Icon name="lucide:arrow-right" class="text-blue-600" />
+            </a>
+          </div>
+          <div class="border border-gray-200 rounded-lg p-4">
+            <NuxtImg src="/assets/landing-page-1/blog3.jpg" class="mb-4" />
+            <p class="text-gray-400 text-lg">Technology</p>
+            <h2 class="text-2xl font-bold my-4">
+              Revenue is targeted to increase by 20%...
+            </h2>
+            <a
+              href="#"
+              class="flex justify-start items-center gap-4 hover:text-blue-600 transition-colors mb-2"
+            >
+              <span class="font-bold">Read More</span>
+              <Icon name="lucide:arrow-right" class="text-blue-600" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
