0e82e82988d22fce25231ec3cc07eb86995d3c70
Lovanirina R <lovanirina.ran@gmail.com>
2025-06-05 07:13:53 +0300

    feat: implement dynamic navigation bar with scroll-based styling

--- pages/landing-page-2/index.vue
+++ pages/landing-page-2/index.vue
@@ -1,14 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavBar from "./nav-bar.vue";
+
+const navbarVariant = ref<"transparent" | "light">("transparent");
+
+onMounted(() => {
+  document.addEventListener("scroll", () => {
+    if (window.scrollY > 5) {
+      navbarVariant.value = "light";
+    } else {
+      navbarVariant.value = "transparent";
+    }
+  });
+});
+</script>
 
 <template>
   <div class="relative">
     <div id="navbar" class="fixed t-0 l-0 w-full z-10">
-      <div
-        class="h-24 w-full text-center flex justify-center items-center font-bold uppercase text-white"
-      >
-        <Icon name="streamline-stickies-color:baby-cart-quality" />
-        <span class="ml-2"> Navigation Bar </span>
-      </div>
+      <NavBar :variant="navbarVariant" />
+      <!-- <NavBar variant="transparent" /> -->
     </div>
     <div class="h-screen">
       <NuxtImg
@@ -17,7 +27,7 @@
         class="h-full object-cover bg-black brightness-75"
       />
     </div>
-    <div class="w-full h-[1000px]">
+    <div class="w-full h-screen">
       <div
         class="w-full h-full bg-slate-200 flex justify-center items-center p-4 text-slate-700"
       >
