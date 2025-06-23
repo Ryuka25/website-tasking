7d59ca8bd4de1c45abbd7eeb0305c322d9c370cc
Lovanirina R <lovanirina.ran@gmail.com>
2025-06-23 07:03:45 +0300

    fix: hmr full page reload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 
   modules: [
-    '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxt/scripts',
-    '@nuxtjs/tailwindcss'
+    '@nuxt/eslint'
   ]
 })
